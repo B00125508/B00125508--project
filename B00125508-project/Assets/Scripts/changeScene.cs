bdf30aa6cfb7409f0b3074c11011bd25a216986a
B00125508 <75093481+B00125508@users.noreply.github.com>
2020-11-28 13:07:03 +0000

    UI added

---
+++ B00125508-project/Assets/Scripts/changeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public string URL = "https://github.com/B00125508/B00125508--project";
+    public void GoToSene1()
+    {
+        SceneManager.LoadScene("Scene 1");
+    }
+
+
+    public void GoToSene2()
+    {
+        SceneManager.LoadScene("Scene 2");
+    }
+    public void loadUrl()
+    {
+        Application.OpenURL(URL);
+    }
+
+}
