4eb49724b8a4255fb5466c9c17b0823417e5a6cb
B00125508 <75093481+B00125508@users.noreply.github.com>
2020-11-30 22:57:14 +0000

    move down

---
+++ B00125508-project/Assets/Scripts/playerController.cs
@@ -0,0 +1,35 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private Rigidbody playerRb;
+    private float zBound = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponen<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector.forward * speed * verticalInput);
+        playerRb.AddForce(Vector.right * speed * horizontalInput);
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+}
